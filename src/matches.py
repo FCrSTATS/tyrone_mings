002a5f2abc662de178e6f300afa48a7d1ed3e89b
FC_rSTATS <35891964+FCrSTATS@users.noreply.github.com>
2020-06-15 17:37:02 +0100

    Update matches.py

--- src/matches.py
+++ src/matches.py
@@ -3,9 +3,6 @@ import numpy as np
 import pandas as pd
 from .tools import *
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 
 def get_time(bgPosCols, bgPosRows, extra_mins):
     mat = np.zeros((13,10))
